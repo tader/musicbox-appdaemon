0711fbf168324c073e6132706fbde805692d6f83
root <root@a0d7b954-ssh.local.hass.io>
2021-04-11 22:23:12 +0200

    Initial Commit

---
+++ musicadmin.py
@@ -0,0 +1,273 @@
+import appdaemon.plugins.hass.hassapi as hass
+from datetime import datetime, timedelta
+import yaml
+import json
+import os
+from typing import Optional, Dict, List
+import contextlib
+import requests
+import re
+
+import sqlite3
+from aiohttp import web
+
+# https://docs.aiohttp.org/en/stable/web_reference.html#request-and-base-request
+
+class Spotify:
+    client_id: str
+    client_secret: str
+    _token: str
+    _expires: int
+
+    def __init__(self, client_id, client_secret):
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self._token = None
+
+    def token(self):
+        if not self._token or datetime.now() >= self._expires:
+            r = requests.post(
+                url='https://accounts.spotify.com/api/token',
+                data={"grant_type": "client_credentials"},
+                auth=(self.client_id, self.client_secret),
+            )
+            d=r.json()
+            self._token = d.get('access_token')
+            self._expires = datetime.now() + timedelta(seconds=d.get('expires_in'))
+        return self._token
+
+    def parse_url(self, url):
+        a = re.compile(
+            f"""
+                ^
+                https://open\.spotify\.com\/
+                ([^\/]+)\/
+                ([^?]+)
+                ([?].*)
+                $
+            """,
+            re.X
+        )
+
+        m = a.match(url)
+
+        if m:
+            return {"type": m.group(1), "id": m.group(2)}
+
+    def data(self, url):
+        item = self.parse_url(url)
+        if item:
+            r = requests.get(
+                url=f'https://api.spotify.com/v1/{item["type"]}s/{item["id"]}',
+                headers={"Authorization": f"Bearer {self.token()}"},
+            )
+            return r.json()
+
+
+
+
+class App(hass.Hass):
+    current: Dict[str,str]
+    cards: "Cards"
+    spotify: "Spotify"
+
+    def initialize(self):
+        self.log(f"Initializing '{self.name}' with args {self.args}")
+        self.cards = Cards()
+        self.spotify = Spotify(
+            self.args['spotify']['client_id'],
+            self.args['spotify']['client_secret'],
+        )
+        self.current = {}
+        for entity in self.args.get('tag_id_entities', []):
+            self.current[entity] = None
+            self.log(f"Listening '{entity}' for tag changes...")
+            self.on_event(entity, None, None, self.get_state(entity), {})
+            self.listen_state(self.on_event, entity)
+
+        self.register_route(self.index)
+
+    def fix_cards(self):
+        for card in self.cards.list():
+            self.set_card_from_spotify_url(card, card.content_id)
+
+    def set_card_from_spotify_url(self, card, url):
+        item = self.spotify.parse_url(url)
+        if item:
+            data = self.spotify.data(url)
+            self.log(data)
+            card.title = ""
+            if "artists" in data:
+                card.title += ", ".join(artist['name'] for artist in data['artists'])
+                card.title += " - "
+            card.title += data['name']
+            if "description" in data:
+                card.description = data['description']
+            card.art = data['images'][0]['url']
+            card.content_id = data['external_urls']['spotify']
+            card.content_type = item['type']
+            self.cards.store(card)
+
+    def on_event(self, entity, attribute, old, new, kwargs):
+        self.log(f"Tag on {entity} changed to: '{new}'")
+        if new not in ("unavailable", "unknown"):
+            self.current[entity] = new
+
+    async def index(self, req):
+        self.log(f"Index({req.path})")
+
+        if 'id' in req.query:
+            return self.web_get_set_card(req)
+        elif 'drop' in req.query:
+            return self.web_drop_card(req)
+        elif 'current' in req.query:
+            return self.web_current_card(req)
+        elif 'list' in req.query:
+            return self.web_list_cards(req)
+        elif 'token' in req.query:
+            return self.web_token(req)
+        elif 'parse' in req.query:
+            return self.web_parse(req, req.query['parse'])
+        else:
+            return self.web_page(req)
+
+    def web_get_set_card(self, req):
+        card = self.cards.get(req.query['id'])
+        if 'content_id' in req.query:
+            self.set_card_from_spotify_url(card, req.query['content_id'])
+        if 'shuffle' in req.query:
+            card.shuffle = req.query['shuffle'] == "on"
+        if 'content_id' in req.query or 'shuffle' in req.query:
+            self.cards.store(card)
+        return web.Response(text=card.json(), content_type="application/json")
+
+    def web_drop_card(self, req):
+        card = self.cards.get(req.query['drop'])
+        if card:
+            self.cards.drop(card)
+        return web.Response(text=card.json(), content_type="application/json")
+
+    def web_current_card(self, req):
+        current_cards = [
+            {current: self.cards.get(current).as_dict() if current else None}
+            for current in self.current.values()
+        ]
+
+        return web.Response(text=json.dumps(current_cards), content_type="application/json")
+
+    def web_list_cards(self, req):
+        #self.fix_cards()
+        all_cards = {
+            card.id: card.as_dict()
+            for card in self.cards.list()
+        }
+
+        return web.Response(text=json.dumps(all_cards), content_type="application/json")
+
+    def web_token(self, req):
+        return web.Response(text=json.dumps(self.spotify.token()), content_type="application/json")
+
+    def web_parse(self, req, url):
+        return web.Response(text=json.dumps(self.spotify.data(url)), content_type="application/json")
+
+    def web_page(self, req):
+        file_name = os.path.join(os.path.dirname(__file__), 'musicadmin.html')
+        with open(file_name, 'r') as ifp:
+            return web.Response(text=ifp.read(), content_type='text/html')
+
+
+class Card:
+    id: str
+    title: Optional[str]
+    description: Optional[str]
+    art: Optional[str]
+    content_type: Optional[str]
+    content_id: Optional[int]
+    shuffle: bool
+
+    def __init__(self, id):
+        self.id = id
+        self.title = None
+        self.description = None
+        self.art = None
+        self.content_type = None
+        self.content_id = None
+        self.shuffle = False
+
+    def as_dict(self) -> str:
+        return {
+            "title": self.title,
+            "description": self.description,
+            "art": self.art,
+            "content_type": self.content_type,
+            "content_id": self.content_id,
+            "shuffle": self.shuffle,
+        }
+
+    def json(self) -> str:
+        return json.dumps(self.as_dict())
+
+    @classmethod
+    def load(cls, id: str, text: str) -> "Card":
+        d = json.loads(text)
+        card = Card(id)
+        card.update_from_json(text)
+        return card
+
+    def update_from_json(self, text: str) -> None:
+        self.update(**json.loads(text))
+
+    def update(self, **data) -> None:
+        for k in ("title", "description", "art", "content_type", "content_id", "shuffle"):
+            if k in data:
+                setattr(self, k, data[k])
+
+    def embed_url(self) -> str:
+        if not self.content_id:
+            return ''
+
+        parts = self.content_id.split('/')
+        return '/'.join(parts[0:3] + ['embed'] + parts[3:])
+
+
+class Cards:
+    def __init__(self):
+        self.file = os.path.join(os.path.dirname(__file__), 'music.db')
+
+    @contextlib.contextmanager
+    def cursor(self):
+        with contextlib.closing(sqlite3.connect(self.file)) as conn:
+            with conn:
+                with contextlib.closing(conn.cursor()) as cursor:
+                    cursor.execute("CREATE TABLE IF NOT EXISTS card (id VARCHAR(32) PRIMARY KEY, metadata TEXT)")
+                with contextlib.closing(conn.cursor()) as cursor:
+                    yield cursor
+
+    def list(self):
+        with self.cursor() as cur:
+            cur.execute("SELECT id, metadata FROM card ORDER BY id")
+            for row in cur.fetchall():
+                yield Card.load(*row)
+
+    def get(self, card_id):
+        with self.cursor() as cur:
+            cur.execute("SELECT id, metadata FROM card WHERE id=?", (card_id,))
+            row = cur.fetchone()
+            if row:
+                return Card.load(*row)
+            else:
+                return Card(card_id)
+
+    def store(self, card:Card) -> None:
+        with self.cursor() as cur:
+            cur.execute(
+                "INSERT OR REPLACE INTO card (id, metadata) VALUES (?, ?)",
+                (card.id, card.json())
+            )
+
+    def drop(self, card:Card) -> None:
+        with self.cursor() as cur:
+            cur.execute(
+                "DELETE FROM card WHERE id = ?",
+                (card.id,)
+            )
---
+++ musicbox.py
@@ -0,0 +1,315 @@
+import appdaemon.plugins.hass.hassapi as hass
+import json
+import os
+from typing import Optional, Dict, List
+import contextlib
+
+import sqlite3
+
+class Card():
+    id: str
+    title: Optional[str]
+    content_type: Optional[str]
+    content_id: Optional[int]
+    shuffle: bool
+
+    def __init__(self, id):
+        self.id = id
+        self.title = None
+        self.content_type = None
+        self.content_id = None
+        self.shuffle = False
+
+    def as_dict(self) -> str:
+        return {
+            "title": self.title,
+            "content_type": self.content_type,
+            "content_id": self.content_id,
+            "shuffle": self.shuffle,
+        }
+
+    def json(self) -> str:
+        return json.dumps(self.as_dict())
+
+    @classmethod
+    def load(cls, id: str, text: str) -> "Card":
+        d = json.loads(text)
+        card = Card(id)
+        card.update_from_json(text)
+        return card
+
+    def update_from_json(self, text: str) -> None:
+        self.update(**json.loads(text))
+
+    def update(self, **data) -> None:
+        if "playlist" in data:
+            self.content_id = data["playlist"]
+            self.content_type = "sonos.playlist"
+
+        for k in ("title", "content_type", "content_id", "shuffle"):
+            if k in data:
+                setattr(self, k, data[k])
+
+
+class Cards():
+    def __init__(self):
+        self.file = os.path.join(os.path.dirname(__file__), 'music.db')
+
+    @contextlib.contextmanager
+    def cursor(self):
+        with contextlib.closing(sqlite3.connect(self.file)) as conn:
+            with conn:
+                with contextlib.closing(conn.cursor()) as cursor:
+                    cursor.execute("CREATE TABLE IF NOT EXISTS card (id VARCHAR(32) PRIMARY KEY, metadata TEXT)")
+                with contextlib.closing(conn.cursor()) as cursor:
+                    yield cursor
+
+    def list(self):
+        with self.cursor() as cur:
+            cur.execute("SELECT id, metadata FROM card")
+            for row in cur.fetchall():
+                yield Card.load(*row)
+
+    def get(self, card_id):
+        with self.cursor() as cur:
+            cur.execute("SELECT id, metadata FROM card WHERE id=?", (card_id,))
+            row = cur.fetchone()
+            if row:
+                return Card.load(*row)
+            else:
+                return Card(card_id)
+
+    def store(self, card:Card) -> None:
+        with self.cursor() as cur:
+            cur.execute(
+                "INSERT OR REPLACE INTO card (id, metadata) VALUES (?, ?)",
+                (card.id, card.json())
+            )
+
+class Player:
+    app: hass.Hass
+    player_entity: str
+
+    def __init__(self, app: hass.Hass, player_entity: str):
+        self.app = app
+        self.player_entity = player_entity
+
+    def log(self, message: str):
+        self.app.log(message)
+
+    def get_state(self, *args, **kwargs):
+        result = self.app.get_state(*args, **kwargs)
+        self.log(f"Get state {args} {kwargs} -> {result}")
+        return result
+        
+    def call(self, *args, **kwargs):
+        self.log(f"Call: {args} {kwargs}")
+        return self.app.call_service(*args, **kwargs)
+
+    def prepare(self):
+        pass
+    
+    def get_media_content_id(self, card: Card) -> str:
+        return card.content_id
+
+    def get_media_content_type(self, card: Card) -> str:
+        return card.content_type
+
+    def play(self, card: Card):
+        self.call(
+            'media_player/shuffle_set',
+            entity_id=self.player_entity,
+            shuffle=card.shuffle,
+        )
+        self.call(
+            'media_player/play_media',
+            entity_id=self.player_entity,
+            media_content_id=self.get_media_content_id(card),
+            media_content_type=self.get_media_content_type(card),
+        )
+
+    def pause(self):
+        self.call(
+            'media_player/media_pause',
+            entity_id=self.player_entity,
+        )
+
+    def resume(self):
+        self.call(
+            'media_player/media_play',
+            entity_id=self.player_entity,
+        )
+
+    def get_volume(self) -> float:
+        return self.get_state(self.player_entity, 'volume_level')
+
+    def set_volume(self, volume: float):
+        self.call(
+            'media_player/volume_set',
+            entity_id=self.player_entity,
+            volume_level=volume,
+        )
+
+
+class SpotifyPlayer(Player):
+    player_source: str
+
+    def __init__(self, app: hass.Hass, player_entity: str, player_source: str):
+        super().__init__(app, player_entity)
+        self.player_source = player_source
+    
+    def prepare(self):
+        current_source = self.get_state(self.player_entity, 'source')
+        self.app.log(f"Current source {current_source} desired source {self.player_source}")
+        if current_source != self.player_source:
+            self.log(f"Changing source to {self.player_source}")
+            self.call(
+                'media_player/select_source',
+                entity_id=self.player_entity,
+                source=self.player_source,
+            )
+
+    def get_media_content_type(self, card: Card) -> str:
+        return "playlist"
+
+
+class SonosPlayer(Player):
+    player_group: List[str]
+
+    def __init__(self, app: hass.Hass, player_entity: str, player_group: List[str]):
+        super().__init__(app, player_entity)
+        self.player_group = player_group
+
+    def players(self) -> List[str]:
+        result = [self.player_entity]
+        if self.player_group:
+            result += self.player_group
+        return result
+
+    def prepare(self):
+        if self.player_group:
+            current_group = self.get_state(self.player_entity, 'sonos_group')
+            if not current_group or set(current_group) != set([self.player_entity] + self.player_group):
+                self.log(f"Grouping SONOS speakers...")
+                self.call(
+                    'sonos/join',
+                    master=self.player_entity,
+                    entity_id=self.player_group,
+                )
+
+    def get_media_content_type(self, card: Card) -> str:
+        return "music"
+
+    def get_volume(self) -> float:
+        volume = .0
+        
+        for player in self.players():
+            player_volume = self.get_state(player, 'volume_level')
+            volume = max(volume, player_volume)
+
+        return volume
+
+    def set_volume(self, volume: float):
+        for player in self.players():
+            self.call(
+                'media_player/volume_set',
+                entity_id=player,
+                volume_level=volume,
+            )
+
+
+class App(hass.Hass):
+    cards: Cards
+    player: Player
+
+    def initialize(self):
+        self.log(f"Initializing '{self.name}' with args {self.args}")
+
+        self.tag_id_entity = self.args['tag_id_entity']
+        self.player_type = self.args.get('player_type', 'default')
+        self.player_entity = self.args['player_entity']
+
+        if self.player_type == "sonos":
+            self.player = SonosPlayer(self, self.player_entity, self.args.get('player_group'))
+        elif self.player_type == "spotify":
+            self.player = SpotifyPlayer(self, self.player_entity, self.args.get('player_source'))
+        else:
+            self.player = Player(self, self.player_entity)
+
+        self.current = None
+        self.playing = None
+
+        self.cards = Cards()
+
+        self.listen_state(self.on_event, self.tag_id_entity)
+        self.log(f"Listening '{self.tag_id_entity}' for tag changes...")
+
+
+    def on_event(self, entity, attribute, old, new, kwargs):
+        self.log(f"Tag changed to: '{new}'")
+        if not new or new == "":
+            self.gone()
+        elif new == "unavailable":
+            pass
+        else:
+            if self.current != new:
+                self.appeared(new)
+
+    def appeared(self, tag_id):
+        self.current = tag_id
+
+        card = self.cards.get(tag_id)
+        self.log(f"Card '{card.as_dict()}' found for '{tag_id}'!")
+
+        if not self.playing or self.playing != card.content_id:
+            # play something new
+            self.playing = None
+            if self.play(card):
+                self.playing = card.content_id
+        else:
+            # unpause
+            self.log("Resume Playing")
+            self.player.prepare()
+            self.ensure_volume()
+            self.player.resume()
+
+    def gone(self):
+        self.log('Tag is gone!')
+        self.current = None
+        self.player.pause()
+
+    def play(self, card) -> bool:
+        content_id = card.content_id
+        if not content_id or content_id == "":
+            return False
+
+        self.log('Play: {}'.format(content_id))
+        
+        self.player.prepare()
+        self.ensure_volume()
+        self.player.play(card)
+
+        return True
+
+    def ensure_volume(self):
+        volume_default = self.args.get('volume_default')
+        volume_max = self.args.get('volume_max')
+        volume_min = self.args.get('volume_min')
+        volume = self.player.get_volume()
+        volume_new = None
+        
+        if volume_default and volume_min and volume_max:
+            if volume > volume_max or volume < volume_min:
+                volume_new = volume_default
+        elif volume_default and not volume_min and not volume_max:
+            volume_new = volume_default
+        else:
+            if volume_max and volume > volume_max:
+                volume_new = volume_max
+            
+            if volume_min and volume < volume_min:
+                volume_new = volume_min
+        
+        if volume_new and volume_new != volume:
+            self.player.set_volume(volume_new)
+
